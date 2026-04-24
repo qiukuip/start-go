5b769d73bbe1e716ff09005b1efcbdfc193a02d0
qiukuip <zhulongkun20@icloud.com>
2026-04-25 00:23:36 +0800

    :sparkles: add database query

--- go-part3/main.go
+++ go-part3/main.go
@@ -4,14 +4,21 @@ import (
 	"database/sql"
 	"fmt"
 	// "os"
-  "log"
+    "log"
 	"github.com/go-sql-driver/mysql"
 )
 
 var db *sql.DB
 
+type Album struct {
+    ID int64
+    Title string
+    Artist string
+    Price float32
+}
+
 func main() {
-  log.SetPrefix("db-access: ")
+    log.SetPrefix("db-access: ")
 	log.SetFlags(0)
 
 	cfg := mysql.NewConfig()
@@ -20,11 +27,11 @@ func main() {
 	cfg.User = "root"
 	cfg.Passwd = "ABab1234!"
 	cfg.Net = "tcp"
-	cfg.Addr = "127.0.0.1:3306"
+	cfg.Addr = "vps:3306"
 	cfg.DBName = "recordings"
 
 	var err error
-	db, err := sql.Open("mysql", cfg.FormatDSN())
+	db, err = sql.Open("mysql", cfg.FormatDSN())
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -35,4 +42,80 @@ func main() {
 	}
 
 	fmt.Println("Connected!")
+
+    // 查询数据
+    albums, err := albumsByArtist("John Coltrane")
+    if err != nil {
+        log.Fatal(err)
+    }
+    fmt.Printf("Albums found: %v\n", albums)
+
+    fmt.Println("============")
+
+    albumItem, err := albumByID(2)
+    if err != nil {
+        log.Fatal(err)
+    }
+    fmt.Printf("Album found: %v\n", albumItem)
+
+    fmt.Println("==============")
+
+    albumId, err := addAlbum(Album{
+        Title: "The Modern Sound of Betty Carter",
+        Artist: "Betty Carter",
+        Price: 49.99,
+    })
+    if err != nil {
+        log.Fatal(err)
+    }
+    fmt.Printf("ID of added album: %+v\n", albumId)
+}
+
+func albumsByArtist(artistName string) ([]Album, error) {
+    var albums []Album
+    rows, err := db.Query("SELECT * FROM album WHERE artist = ?", artistName)
+    if err != nil {
+        return nil, fmt.Errorf("albumsByArtist %q: %v", artistName, err)
+    }
+
+    defer rows.Close()
+    for rows.Next() {
+        var albumItem Album
+        if err := rows.Scan(&albumItem.ID, &albumItem.Title, &albumItem.Artist, &albumItem.Price); err != nil {
+            return nil, fmt.Errorf("albumsByArtist %q: %v", artistName, err)
+        }
+        albums = append(albums, albumItem)
+    }
+
+    if err := rows.Err(); err != nil {
+        return nil, fmt.Errorf("albumsByArtist, %q: %v", artistName, err)
+    }
+    
+    return albums, nil
+}
+
+func albumByID(id int64) (Album, error) {
+    var albumItem Album
+    row := db.QueryRow("SELECT * FROM album WHERE id = ?", id)
+    if err := row.Scan(&albumItem.ID, &albumItem.Title, &albumItem.Artist, &albumItem.Price); err != nil {
+        if err == sql.ErrNoRows {
+            return albumItem, fmt.Errorf("albumsByID %d: no sub album", id)
+        }
+        return albumItem, fmt.Errorf("albumsByID %d: %v", id, err)
+    }
+    return albumItem, nil
+}
+
+func addAlbum(album Album) (int64, error) {
+    result, err := db.Exec("INSERT INTO album (title, artist, price) values (?, ?, ?)", album.Title, album.Artist, album.Price)
+    if err != nil {
+        return 0, fmt.Errorf("addAlbum: %+v", err)
+    }
+
+    id, err := result.LastInsertId()
+    if err != nil {
+        return 0, fmt.Errorf("addAlbum, %+v", err)
+    }
+
+    return id, nil
 }
